22398603abee776ca137f7fc4590ca1167b4e973
peterdinis <petter1@pobox.sk>
2025-04-05 12:23:57 +0200

    infra: Buidling new resend module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { AppResendModule } from 'src/resend/resend.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AppResendModule],
   controllers: [AppController],
   providers: [AppService],
 })
