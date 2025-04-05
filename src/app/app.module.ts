8621edd4f0cea8dbb7b0ab52a51203bf109df128
peterdinis <petter1@pobox.sk>
2025-04-05 12:26:02 +0200

    feat: Prepare config.module for future

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AppResendModule } from 'src/resend/resend.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AppResendModule],
+  imports: [PrismaModule, AppResendModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
