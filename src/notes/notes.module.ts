32b69d8d31a0ffcd7cf1f2b7182ba52d04d7f089
peterdinis <petter1@pobox.sk>
2025-04-06 16:01:53 +0200

    infra: Update Notes.module

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { NotesService } from './notes.service';
+import { NotesController } from './notes.controller';
 
 @Module({
   imports: [PrismaModule],
+  providers: [NotesService],
+  controllers: [NotesController]
 })
 export class NotesModule {}
