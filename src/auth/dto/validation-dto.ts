e2a0b3f2644477d42001be56b3cc56933abf5204
peterdinis <petter1@pobox.sk>
2025-04-06 12:23:55 +0200

    infra: Working on auth WIP

---
+++ src/auth/dto/validation-dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class ValidationDto {
+    @IsString()
+    @IsNotEmpty()
+    sub: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+}
