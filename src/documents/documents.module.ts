588428952d8ce0f7c53978636a5c64b6a749e051
peterdinis <petter1@pobox.sk>
2025-04-11 20:09:15 +0200

    infra: Start working on documents logic WIP

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
+import { DocumentsController } from "./documents.controller";
+import { DocumentsService } from "./documents.service";
 
 @Module({
-    imports: [PrismaModule]
+    imports: [PrismaModule],
+    controllers: [DocumentsController],
+    providers: [DocumentsService]
 })
 
 export class DocumentsModule {}
